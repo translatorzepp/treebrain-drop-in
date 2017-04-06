27deab2edf9dd8f5dd6824272568a834cb1d7384
zpalmer <zoe.palmer@getbraintree.com>
2017-04-06 12:08:14 -0500

    add route methods

--- treebraindropin.py
+++ treebraindropin.py
@@ -12,6 +12,35 @@ app.config.update(dict(
     BRAINTREE_PRIVATE_KEY='7dd7253c4c53d675f15e869212659579'
 )) #set config environment variables
 
+
+@app.route('/', methods=["GET"])
+def get_client_token():
+    client_token = braintree.ClientToken.generate()
+    return render_template('checkout.html', client_token=client_token)
+
+@app.route('/print_client_token', methods=["GET"])
+def print_client_token():
+    client_token = braintree.ClientToken.generate()
+    return client_token
+    # to do: add display of decoded client token
+    # return render_template('client_token.html', client_token=client_token)
+
+@app.route('/nonce_received', methods=['POST'])
+def store_nonce():
+    nonce = request.form["nonce"]
+    db = get_db()
+    db.execute('insert into nonces (nonce) values (?)', nonce)
+    db.commit()
+    return nonce
+    # to do: add real display of nonce / more stuff
+    # return render_template('nonce_received.html', nonce=nonce)
+
+@app.route('/past_nonces')
+def show_nonces():
+    db = get_db()
+    query = db.execute('select pk, nonce, time from nonces order by time asc')
+
+
 # Database stuff
 def connect_db():
     """Connects to the specific database."""
